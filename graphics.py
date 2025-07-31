ae7d4930a2f1eb6ac453256013205dc94d91e642
Stefano Labianca <lstefano@proton.me>
2025-07-31 13:37:07 +0200

    You can now create a window and adding some lines

---
+++ graphics.py
@@ -0,0 +1,49 @@
+from tkinter import Tk, BOTH, Canvas
+from typing import Literal
+
+class Point:
+    def __init__(self, x: float, y: float):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, p1: Point, p2: Point):
+        self.__p1 = p1
+        self.__p2 = p2
+    
+    def draw(self, canvas: Canvas, fill_color: str):
+        if fill_color != "red" and fill_color != "black":
+            raise ValueError("fill_color should be red or black")
+        
+        canvas.create_line(self.__p1.x, self.__p1.y, self.__p2.x,self.__p2.y, fill=fill_color, width=2)
+
+
+class Window:
+    def __init__(self, width: int, height: int):
+        self.__root = Tk()
+        self.__root.title("Maze Solver")
+        
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__canvas.pack(fill=BOTH, expand=1)
+        
+        self.__is_window_running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.__is_window_running = True
+
+        while self.__is_window_running:
+            self.redraw()
+        print("window closed...")
+
+    def close(self):
+        self.__is_window_running = False
+
+    def draw_line(self, line: Line, fill_color: Literal["red", "black"] = "black"):
+        line.draw(self.__canvas, fill_color)
+
+
--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from maze-solver!")
+from graphics import  Line, Point, Window
 
+
+def main():
+    win = Window(800, 600)
+
+    line1 = Line(Point(10, 10), Point(500, 100))
+    line2 = Line(Point(400, 400), Point(100, 100))
+    
+    win.draw_line(line1, 'red')
+    win.draw_line(line2)
+    
+    win.wait_for_close()
 
 if __name__ == "__main__":
     main()
