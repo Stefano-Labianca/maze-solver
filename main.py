f084103015270b59b27cd309f80fb3c9c33b4812
Stefano Labianca <lstefano@proton.me>
2025-07-31 18:08:04 +0200

    removed magic numbers

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
-from graphics import  Cell, Maze, Window
+from graphics import  Maze, Window
 
 
 ROWS = 20
 COLUMNS = 30
 CELL_SIZE_X = 20
 CELL_SIZE_Y = 20
+MAZE_X = 4
+MAZE_Y = 4
 
 def main():
     win = Window(800, 600)
-    maze = Maze(4, 4,
-        ROWS, COLUMNS, CELL_SIZE_X, CELL_SIZE_Y , win
-    )
+    maze = Maze(MAZE_X, MAZE_Y,ROWS, COLUMNS, CELL_SIZE_X, CELL_SIZE_Y , win)
     
-
-
     win.wait_for_close()
 
 if __name__ == "__main__":
