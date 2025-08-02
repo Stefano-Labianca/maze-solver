8a8b0c00a60133a594261189cf11dbedd8765132
Stefano Labianca <lstefano@proton.me>
2025-08-02 12:26:55 +0200

    improvement on the main application

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 from graphics import Window
 from maze import Maze
+from math import floor
+import sys
 
-
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
 ROWS = 15
 COLUMNS = 20
-CELL_SIZE_X = 30
-CELL_SIZE_Y = 30
-MAZE_X = 10
-MAZE_Y = 10
+MAZE_MARGIN_X = 10
+MAZE_MARGIN_Y = 10
+
+ANIMATION_SPEED = 0.01
+SEED = 0
+
+RECURSION_LIMIT = 10000
 
 def main():
-    win = Window(800, 600)
-    maze = Maze(MAZE_X, MAZE_Y,ROWS, COLUMNS, CELL_SIZE_X, CELL_SIZE_Y , win)
+    sys.setrecursionlimit(10000)
+    win = Window(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+    cell_size_x = floor((SCREEN_WIDTH - 2 * MAZE_MARGIN_X) / COLUMNS)
+    cell_size_y = floor((SCREEN_HEIGHT - 2 * MAZE_MARGIN_Y) / ROWS)
+
+    maze = Maze(MAZE_MARGIN_X, MAZE_MARGIN_Y, ROWS, COLUMNS, cell_size_x, cell_size_y , win, SEED, ANIMATION_SPEED)
+    
+    print(f"Solved? {maze.solve()}")
+
 
     win.wait_for_close()
 
